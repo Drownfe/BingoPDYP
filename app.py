b8cb6a4c2d0ba41bb4675696854f6ce05da9eb68
Drownfe <palaciojuanfe@gmail.com>
2025-11-20 21:08:26 -0500

    Nuevo Readme y ajustes app

--- app.py
+++ app.py
@@ -230,4 +230,4 @@ def handle_start_game():
 
 # Punto de entrada
 if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
